1ceb2458922536a940372df2c0b611448398b241
oktaviana1007 <38560190+oktaviana1007@users.noreply.github.com>
2018-05-22 06:33:52 +0700

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -38,46 +38,46 @@ void bubble(int a[],int x){
 }
 
 void mergeSort(int data[],int x){ 
- int a[x],b[x];
+int a[x],b[x];
  if(x=2){ 
- if(data[0]>data[1]){ 
-  int p=data[0]; 
-  data[0]=data[1]; 
-  data[1]=p; 
+  if(data[0]>data[1]){ 
+   int p=data[0]; 
+   data[0]=data[1]; 
+   data[1]=p; 
+  } 
  } 
-} 
  else if(x>2){ 
   int m=(x/2); 
-  for(int i=0;i<=m-1;i++){ 
-   a[i]=data[i]; 
-  } 
-  for(int i=m;i<=x-1;i++){ 
-   b[i-m]=data[i]; 
-  } 
+   for(int i=0;i<=m-1;i++){ 
+    a[i]=data[i]; 
+   } 
+   for(int i=m;i<=x-1;i++){ 
+    b[i-m]=data[i]; 
+   } 
   mergeSort(a,m); 
   mergeSort(b-x,m); 
   int i=0; 
   int j=0; 
-  for(int k=0;k<=x-1;k++){ 
-   if(a[i]<=b[j]){ 
-    data[k]=a[i]; 
-    i=i+1; 
-   }
-else{
-    data[k]=b[j]; 
-    j=j+1; 
+   for(int k=0;k<=x-1;k++){ 
+    if(a[i]<=b[j]){ 
+     data[k]=a[i]; 
+     i=i+1; 
+    }
+    else{
+     data[k]=b[j]; 
+     j=j+1; 
+    } 
    } 
   } 
- } 
-}
+ }
 
 void tampil(int a[],int x){
  
- cout<<endl;
- for(int i=0;i<x;i++){
- cout<<a[i]<<" "; 
- }
- cout<<endl;
+cout<<endl;
+  for(int i=0;i<x;i++){
+   cout<<a[i]<<" "; 
+  }
+cout<<endl;
 }
 
 int main(){
@@ -85,9 +85,9 @@ int n;
 cin>>n;
 cout<<endl;
 int a[n];
-for(int i=0;i<n;i++){
-cin>>a[i];
-}
+  for(int i=0;i<n;i++){
+   cin>>a[i];
+  }
 selection(a,n);
 tampil(a,n);
 bubble(a,n);
